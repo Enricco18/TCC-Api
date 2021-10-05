c7576a5b35a5f91ad67142c6cdfd782f4a6f0578
enric <enric@DESKTOP-A95K7RB>
2021-10-05 14:42:12 -0300

    Crooss origin resolved

--- src/main/java/br/com/pichau/api/controllers/BalanceController.java
+++ src/main/java/br/com/pichau/api/controllers/BalanceController.java
@@ -13,10 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
